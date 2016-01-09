78ef064823b48870a1f4d9497ba7f22b593e4db8
José Manuel Rodríguez <Jmrodriguez90@correo.ugr.es>
2016-01-09 13:07:04 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,24 +1,7 @@
 #!/usr/bin/env nodejs
 
 /*
-	CalificaEmpresas. Aplicación de ejemplo para los ejercicios del tema 1 de
-  Cloud Computing del Máster Universitario en Ingeniería Informática.
-	Copyright (C) 2015 Germán Martínez Maldonado
-
-	This file is part of CalificaEmpresas.
-
-	CalificaEmpresas is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	CalificaEmpresas is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program. If not, see <http://www.gnu.org/licenses/>.
+Código de [Germán Martínez](https://github.com/germaaan), usado por [José M. Rodríguez](https://github.com/jmrodriguez90) para la realización de los ejercicios de la clase de Cloud Computing del Máster en Ingeniería informática de la UGR
 */
 
 
